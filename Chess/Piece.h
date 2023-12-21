a5115f9f982b159a3d82825054207e283271f3f9
Lihi_Verov <verovlihi@gmail.com>
2023-12-21 16:16:37 +0200

    Added Piece.h file

--- Chess/Piece.h
+++ Chess/Piece.h
@@ -1,1 +1,20 @@
 #pragma once
+
+enum Types {TOWER, KING, QUEEN, BISHOP, KNIGHT, PAWN };
+enum Colors {WHITE, BLACK};
+
+class Piece
+{
+private:
+	//int _letterLocation;
+	//int _numberLocation;
+	int _color;
+	int _type;
+public:
+	Piece(int color, int type, int row, int col);
+	virtual ~Piece();
+	int getType();
+	int getColor();
+	virtual bool canBeMoved(int sourceRow, int sourceCol, int dest, int destCol) = 0;
+	virtual bool canEatKing(int row, int col) = 0;
+};
