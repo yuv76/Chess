2097f79f35ef888007406881b31ba0f6a187d595
yuv76 <u.val.orian@gmail.com>
2023-12-24 17:37:52 +0200

    some turn changes

--- Chess/Game.cpp
+++ Chess/Game.cpp
@@ -63,35 +63,65 @@ void Game::sendNextMove()
 
 /*
 
+*/
 void Game::playGame(Colors FirstPlayer)
 {
 	std::string msg = "";
-	bool legalMsg = false;
+	bool legalTurn = false;
+	MsgCode status;
+	int* extractedMsg = nullptr;
+
 
 	//start game by sending board to front
 
 	while (this->getWinState() == UNDETERMINED)
 	{
 		this->_gameBoard.printBoard();
-		while (!legalMsg)
+		while (!legalTurn)
 		{
-			std::cin >> msg;
-			legalMsg = this->checkMsg(msg);
+			extractedMsg = this->checkMsg(msg, status);
+			turnStatus = this->turn(extractedMsg, legalTurn);
 		}
-		this->turn(msg);
+		
+	}
+	if (this->getWinState() == WHITE)
+	{
+		std::cout << "white won!";
+	}
+	else
+	{
+		std::cout << "black won!";
 	}
 }
-*/
 
 /*
-MsgCode Game::turn(std::string move)
+
+*/
+MsgCode Game::turn(int* move, bool& validFlag)
 {
+	bool valid = false;
 
+	//make move
+	this->_gameBoard.move(move[0], move[1], move[2], move[3], this->_turn);
+
+	//next turn
+	if (this->_turn == BLACK)
+	{
+		this->_turn = WHITE;
+	}
+	else
+	{
+		this->_turn = BLACK;
+	}
 }
-*/
+
 
 /*
-MsgCode Game::checkMsg(std::string msg)
+checks if massage is a valid move, returns the message as an array if it is
+input: the massege as string, a message code to change according to the vlidity status
+output: an int array pointer in size of 4, each cell contains the massege positions
+*/
+int* Game::checkMsg(std::string msg, MsgCode& code)
 {
 	int extractedMsg[4] = { 0 };
 	MsgCode valid = VALID;
@@ -105,6 +135,16 @@ MsgCode Game::checkMsg(std::string msg)
 		extractedMsg[2] = msg[2] - ASCII_SUB_FOR_COL;
 		extractedMsg[3] = msg[3] - ASCII_SUB_FOR_ROW;
 
+		//check if the source and destination are different
+		if (msg[0] != msg[2] && msg[1] != msg[3])
+		{
+			return VALID; //not final at all/
+		}
+		else
+		{
+			return SAME_POS;
+		}
+		/*
 		//check if a chess occured.
 		valid = this->_gameBoard.checkIfChess(msg);
 		//check if source position has player's piece
@@ -119,14 +159,15 @@ MsgCode Game::checkMsg(std::string msg)
 
 		//check if checkmate
 		valid = this->_gameBoard.checkIfCheckmate(msg);
+		*/
 	}
 	else
 	{
-		valid = INVALID_INDEXES;
+		return INVALID_INDEXES;
 	}
 
 	//send 
 }
-*/
+
 
 
